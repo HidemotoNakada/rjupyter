d5f4d6818c641c6bfe8421304e1f6c1404ebcf60
HidemotoNakada <hidemoto.nakada@gmail.com>
2021-01-06 10:31:02 +0900

    add client

---
+++ src/rjupyter_client.py
@@ -0,0 +1,141 @@
+import logging
+import json
+import sys, os
+import subprocess
+import socketserver
+import threading
+import tempfile
+from pathlib import Path
+
+FORMAT='%(asctime)s CLIENT %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('mainlogger')
+logger.setLevel(logging.INFO)
+
+DEFAULT_SERVER="localhost"
+SSH_COMMAND="ssh"
+SSH_OPTIONS={"controlmaster":"auto", "controlpath":None}
+RJUPYTER_SERVER="rjupyter_server"
+RCWD="."
+
+def gen_ssh_options(sock_file):
+    SSH_OPTIONS["controlpath"] = sock_file
+    options = []
+    for k, v in SSH_OPTIONS.items():
+        options.append("-o")
+        options.append("%s=%s"%(k,v))
+    logger.info(str(options))
+    return options
+
+def gen_sock_file():
+    ssh_dir = str(Path.home())+"/.ssh/"
+    f = tempfile.NamedTemporaryFile(prefix=ssh_dir)
+    f.close()
+    #os.remove(f.name)
+    return f.name
+
+class ServerStub(object):
+    def __init__(self, server):
+        self.server = server
+        self.sock_file = gen_sock_file()
+        opts = gen_ssh_options(self.sock_file)
+        self.proc = subprocess.Popen(
+                        [SSH_COMMAND, *opts, server, RJUPYTER_SERVER], 
+                        stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE
+                    )
+        self.to_server = self.proc.stdin
+        self.from_server = self.proc.stdout
+        self.server_err = self.proc.stderr
+        self.redirect_thtread = threading.Thread(target=self.redirect_stderr)
+        self.redirect_thtread.start()
+
+    def invoke_jupyter(self):
+        return ("localhost", 8888)
+    def test(self):
+        test_cmd_dict = {"cmd":"test"}
+        self.send(test_cmd_dict)
+        ack = self.recv()
+        assert(ack["code"] == "OK")
+    def set(self, dict):
+        cmd_dict = {"cmd":"set", "vals":dict}
+        self.send(cmd_dict)
+        ack = self.recv()
+        assert(ack["code"] == "OK")
+    def exec(self):
+        cmd_dict = {"cmd":"exec"}
+        self.send(cmd_dict)
+        ack = self.recv()
+        assert(ack["code"] == "OK")
+        logger.info("port:%s", ack["val"]["port"])
+        self.target_port = ack["val"]["port"]
+        self.target_host = ack["val"]["host"]
+        self.token = ack["val"]["token"]
+
+    def add_forward(self, local_port):
+        subprocess.run(
+            [SSH_COMMAND, "-O", "forward", "-L", 
+            "%d:%s:%s"%(local_port, self.target_host, self.target_port),
+            "-S", self.sock_file,
+            self.server
+            ]
+        )
+
+    def send(self, cmd_dict):
+        cmd_str = json.dumps(cmd_dict).encode()
+        self.to_server.write(cmd_str)
+        self.to_server.write(b'\n')
+        self.to_server.flush()
+        logger.info("sending: %s", cmd_str)
+
+    def recv(self):
+        ack_str = self.from_server.readline().decode().strip()
+        logger.info("recv: %s", ack_str)
+        return json.loads(ack_str)
+
+    def redirect_stderr(self):
+        while True:
+            one_line = self.server_err.readline()
+            if len(one_line) == 0:
+                break
+            sys.stderr.write(one_line.decode())
+            sys.stderr.flush()        
+
+def find_vacant_port(port_base, trial_count):
+    """find vacant port for forwarding, 
+    starts with port_base and test at most trial_count ports.
+    if it cannot find, it raise OSError"""
+    for i in range(trial_count):
+        try:
+            ss = socketserver.TCPServer(('127.0.0.1', port_base + i), None)
+            # found one
+            ss.server_close()
+            return port_base + i
+        except OSError:
+            pass
+    raise OSError("cannot find vacant port")
+
+def open_browser(port, tokenstring):
+    subprocess.run(
+        [
+            "open",
+            "http://localhost:%d/?%s"%(port,tokenstring)
+        ]
+    )
+
+def main():
+    server = ServerStub(DEFAULT_SERVER)
+    server.test()
+    server.set({
+        "cwd": RCWD
+    })
+    server.exec()
+#    host_port = server.invoke_jupyter()
+    local_port = find_vacant_port(9000, 100)
+    logger.info("found local port %s", local_port)
+    server.add_forward(local_port)
+    open_browser(local_port, server.token)
+
+if __name__ == "__main__":
+    main()
--- src/rjupyter_server.py
+++ src/rjupyter_server.py
@@ -3,6 +3,12 @@ import logging
 import json
 import sys
 import os, signal
+import subprocess
+import urllib
+import urllib.parse
+import threading
+import atexit
+JUPYTER_CMD = "jupyter"
 
 FORMAT='%(asctime)s SERVER %(message)s'
 logging.basicConfig(format=FORMAT)
@@ -10,6 +16,50 @@ logger = logging.getLogger('mainlogger')
 logger.setLevel(logging.INFO)
 
 values = {}
+
+class JupyterStub(object):
+    def __init__(self):
+        pass
+
+    def start(self, cmd_dict):
+        if "cwd" in values.keys():
+            os.chdir(values["cwd"])
+        self.proc = subprocess.Popen(
+                        [JUPYTER_CMD, "notebook"], 
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE
+                    )   
+        self.from_jupyter = self.proc.stdout
+        self.jupyter_err = self.proc.stderr   
+        self.redirect_thtread = threading.Thread(target=self.redirect_stderr)
+        atexit.register(self.kill_jupyter)
+
+    def kill_jupyter(self):
+        self.proc.kill()
+
+    def find_jupyter_url(self):
+        pick_next = False
+        while True:
+            line = self.jupyter_err.readline().decode().strip()
+            logger.info(line)
+            if pick_next:
+                tokens = line.split()
+                o = urllib.parse.urlparse(tokens[3])
+                break                 
+            if "The Jupyter Notebook is running at:" in line:
+                pick_next = True
+        self.redirect_thtread.start()            
+        return {"port": o.port, "host": "localhost", "token": o.query}
+
+    def redirect_stderr(self):
+        while True:
+            one_line = self.jupyter_err.readline().decode().strip()
+            logger.info("stderr: %s", one_line)
+            if len(one_line) == 0:
+                break
+#            sys.stderr.write(one_line)
+#            sys.stderr.flush()            
+
 
 class ProcManager(object):
     def __init__(self):
@@ -21,24 +71,36 @@ class ProcManager(object):
         for pid in self.pids:
             os.kill(pid, signal.SIGTERM)
 
-class CmdErroException(Exception):
+class CmdErrorException(Exception):
     pass
+
+def gen_ack(code, val=None):
+    return {"code": code, "val": val}
 
 class Cmd(object):
     def __init__(self, json_string):
         self.obj = json.loads(json_string)
+        logger.info("%s", str(self.obj))
         if type(self.obj) != dict:
             raise CmdErrorException(json_string)
-        if not self.obj.has_key("cmd")
+        if not "cmd" in self.obj.keys():
             raise CmdErrorException(json_string)
 
-    def exec(self):
+    def proc(self):
         cmd = self.obj["cmd"]
         if cmd == "set":
             values.update(self.obj["vals"])
-            return "OK"
+            return gen_ack("OK")
         if cmd == "exec":
-            return "OK"
+            res = self.start_jupyter()
+            return gen_ack("OK", res)
+        if cmd == "test":
+            return gen_ack("OK")
+
+    def start_jupyter(self):
+        jupyter = JupyterStub()
+        jupyter.start({})
+        return jupyter.find_jupyter_url()
 
 def main():
     logger.info('startup.')
@@ -47,8 +109,8 @@ def main():
     try:
         while True:
             cmd = Cmd(sys.stdin.readline())
-            res = cmd.exec()
-            sys.stdout.write(res)
+            res = cmd.proc()
+            sys.stdout.write(json.dumps(res))
             sys.stdout.write("\n")
             sys.stdout.flush()
     except json.decoder.JSONDecodeError as e:
@@ -57,7 +119,10 @@ def main():
         logger.error('unknown error : %s', e.msg)
     pm.kill_all()
 
-
+def test():
+    jupyter = JupyterStub()
+    jupyter.start({})
+    print(jupyter.find_jupyter_url())
 
 if __name__ == "__main__":
     main()
